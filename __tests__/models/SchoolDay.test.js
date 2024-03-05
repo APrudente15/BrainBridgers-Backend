f1c4164731ee585d6a2eba67f41084e5519c2363
Jessica-000 <bolu88@outlook.com>
2024-03-05 14:41:06 +0000

    Update tests in models school day

--- __tests__/models/SchoolDay.test.js
+++ __tests__/models/SchoolDay.test.js
@@ -1,7 +1,7 @@
 const SchoolDay = require('../../models/SchoolDay');
 const db = require('../../database/connect');
 
-jest.mock('../../database/connect'); // Mocking the database connection
+jest.mock('../../database/connect')
 
 describe("SchoolDay", () => {
     describe("getCurrentSchoolDayForStudent", () => {
@@ -10,15 +10,12 @@ describe("SchoolDay", () => {
       });
   
       it("should return the current school day for a student", async () => {
-        // Mocking the database query result
-        const mockRows = [{ id: 1, date: new Date(), lesson1_id: "Maths", /* Add other columns as needed */ }];
+        const mockRows = [{ id: 1, date: new Date(), lesson1_id: "Maths"}];
         db.query.mockResolvedValueOnce({ rows: mockRows });
-  
-        // Call the method
+
         const studentId = 1;
         const result = await SchoolDay.getCurrentSchoolDayForStudent(studentId);
-  
-        // Expectations
+
         expect(result).toEqual(mockRows[0]);
         expect(db.query).toHaveBeenCalledWith(
           expect.any(String),
@@ -27,15 +24,12 @@ describe("SchoolDay", () => {
       });
   
       it("should throw an error when database query fails", async () => {
-        // Mocking a database error
-        const mockError = new Error("Database query failed");
-        db.query.mockRejectedValueOnce(mockError);
+        const mockError = new Error("Database query failed")
+        db.query.mockRejectedValueOnce(mockError)
   
-        // Call the method
         const studentId = 1;
         await expect(SchoolDay.getCurrentSchoolDayForStudent(studentId)).rejects.toThrowError(mockError);
   
-        // Expectations
         expect(db.query).toHaveBeenCalledWith(
           expect.any(String),
           expect.arrayContaining([studentId])
