628476a5e026717a86a444372d5643a06009fe7b
hortfrancis <ajhf@hotmail.co.uk>
2024-03-05 18:28:55 +0000

    Improve GET SchoolDay for Student
    - SchoolDay model returns a SchoolDay object
    - Add constructor to SchoolDay
    - schoolDay controller returns an object with a key 'schoolName'
    - Fix missing 0 in seeder SQL

--- models/SchoolDay.js
+++ models/SchoolDay.js
@@ -1,6 +1,25 @@
 const db = require("../database/connect");
 
 class SchoolDay {
+    constructor({
+        id,
+        studentId,
+        date,
+        lesson1_id,
+        lesson2_id,
+        lesson3_id,
+        lesson4_id,
+        lesson5_id
+    }) {
+        this.id = id;
+        this.studentId = studentId;
+        this.date = date.toISOString().split('T')[0];  // YYYY-MM-DD (without time) 
+        this.lesson1_id = lesson1_id;
+        this.lesson2_id = lesson2_id;
+        this.lesson3_id = lesson3_id;
+        this.lesson4_id = lesson4_id;
+        this.lesson5_id = lesson5_id;
+    }
 
     static async getCurrentSchoolDayForStudent(studentId) {
         try {
@@ -16,7 +35,7 @@ class SchoolDay {
             `;
             const values = [studentId];
             const { rows } = await db.query(query, values);
-            return rows[0];
+            return new SchoolDay(rows[0]);
         } catch (error) {
             console.error('Error in `models/SchoolDay.getCurrentSchoolDayForStudent`:', error);
             throw error;
