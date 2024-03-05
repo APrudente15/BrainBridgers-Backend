fbe978d20dd06dce8032434098765f2480becb03
Jessica-000 <bolu88@outlook.com>
2024-03-05 15:59:58 +0000

    Add updates to seed database

--- database/seed.sql
+++ database/seed.sql
@@ -1,7 +1,3 @@
-DROP TABLE IF EXISTS SchoolDay CASCADE;
-DROP TABLE IF EXISTS Subject CASCADE;
-DROP TABLE IF EXISTS Student CASCADE;
-
 INSERT INTO SchoolDay
   (date, lesson1_id, lesson2_id, lesson3_id, lesson4_id, lesson5_id)
 VALUES
@@ -10,6 +6,15 @@ VALUES
 ('2024-03-06', '3', '1', '11', '4', '13'),
 ('2024-03-07', '2', '9', '5', '1', '10'),
 ('2024-03-08', '2', '7', '12', '13', '3');
+
+INSERT INTO Lesson
+  (student_id, confidence, enjoyment)
+VALUES
+('1', '3', '3')
+('5', '2', '2')
+('9', '3', '1')
+('13', '2', '3')
+('11', '1', '1')
 
 INSERT INTO Subject
   (name)
